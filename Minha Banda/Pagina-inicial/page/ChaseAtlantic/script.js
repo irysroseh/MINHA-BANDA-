c79be5cf777c2a6a87e8ddecb7a736357e575d65
irys rhose <109668853+irysroseh@users.noreply.github.com>
2023-04-19 09:14:16 -0300

    Add files via upload

---
+++ Minha Banda/Pagina-inicial/page/ChaseAtlantic/script.js
@@ -0,0 +1,12 @@
+function hideModal                                                                    () {
+    document.getElementById("modal").style.display = "none";
+    document.getElementById("noodle1").style.display = "block";
+    document.getElementById("noodle2").style.display = "block";
+  }
+  function showModal() {
+    document.getElementById("modal").style.display = "flex";
+    document.getElementById("noodle1").style.display = "none";
+    document.getElementById("noodle2").style.display = "none";
+  }
+  
+ 
